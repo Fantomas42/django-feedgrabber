2c8e6b65fc49aebfe755183a8ad8b2b190164e47
Fantomas42 <fantomas42@gmail.com>
2009-09-04 17:38:15 +0200

    adding pagination

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 """Settings for feedgrabber"""
 from django.conf import settings
 
+PAGINATION = getattr(settings, 'FEEDGRABBER_PAGINATION', 20)
 UPDATE_HOUR_LIMIT = getattr(settings, 'FEEDGRABBER_UPDATE_HOUR_LIMIT', 24)
 RECENT_HOUR_LIMIT = getattr(settings, 'FEEDGRABBER_RECENT_HOUR_LIMIT', 48)
 USER_AGENT = getattr(settings, 'FEEDGRABBER_USER_AGENT', 'FeedGrabber/0.1b')
--- urls/items.py
+++ urls/items.py
@@ -2,13 +2,19 @@
 from django.conf.urls.defaults import *
 
 from feedgrabber.models import Item
+from feedgrabber.settings import PAGINATION
 
-item_conf = {'queryset': Item.objects.all()}
+item_conf = {'queryset': Item.objects.all(),
+             'paginate_by': PAGINATION}
+
+item_conf_detail = {'queryset': Item.objects.all(),}
 
 urlpatterns = patterns('django.views.generic.list_detail',
                        url(r'^$', 'object_list',
                            item_conf, 'feedgrabber_item_list'),
+                       url(r'^page/(?P<page>\d)/$', 'object_list',
+                           item_conf, 'feedgrabber_item_list_paginated'),                       
                        url(r'^(?P<slug>[-\w]+)/$', 'object_detail',
-                           item_conf, 'feedgrabber_item_detail'),
+                           item_conf_detail, 'feedgrabber_item_detail'),
                        )
 
