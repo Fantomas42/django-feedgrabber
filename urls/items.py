d655ab407bea04c5d6a4db9b6b4b2150359b81e7
Fantomas42 <fantomas42@gmail.com>
2009-12-14 17:25:08 +0100

    correcting urls

--- urls/items.py
+++ urls/items.py
@@ -12,7 +12,7 @@ item_conf_detail = {'queryset': Item.objects.all(),}
 urlpatterns = patterns('django.views.generic.list_detail',
                        url(r'^$', 'object_list',
                            item_conf, 'feedgrabber_item_list'),
-                       url(r'^page/(?P<page>\d)/$', 'object_list',
+                       url(r'^page/(?P<page>\d+)/$', 'object_list',
                            item_conf, 'feedgrabber_item_list_paginated'),                       
                        url(r'^(?P<slug>[-\w]+)/$', 'object_detail',
                            item_conf_detail, 'feedgrabber_item_detail'),
